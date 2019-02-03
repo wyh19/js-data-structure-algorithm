c5bccbf23ec23527efbe21b5c0b981f18365c129
王焱辉 <wyh_19@163.com>
2019-02-03 09:56:24 +0800

    练习

--- 3-栈/4-ES6创建栈3.js
+++ 3-栈/4-ES6创建栈3.js
@@ -1,17 +1,24 @@
-const items = new WeakMap();
-class Stack {
-    constructor() {
-        items.set(this, []);
+let Stack = (function () {
+    //有一种数据类型可以确保属性是私有的，这就是WeakMap
+    const items = new WeakMap();
+    class Stack {
+        constructor() {
+            //以this(Stack类自己的引用)为键，把代表栈的数组存入items
+            items.set(this, []);
+        }
+        push(element) {
+            // 从WeakMap中取出值，即以this为键从items中取值。
+            let s = items.get(this);
+            s.push(element);
+        }
+        pop() {
+            let s = items.get(this);
+            let r = s.pop();
+            return r;
+        }
     }
-    push(element) {
-        let s = items.get(this);
-        s.push(element);
-    }
-    pop() {
-        let s = items.get(this);
-        let r = s.pop();
-        return r;
-    }
-}
+    return Stack
+})()
 let s1 = new Stack()
 console.log(s1)
+//以上实现了栈的数据封装
