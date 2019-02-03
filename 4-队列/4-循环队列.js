d61a272a5b65600016d10c1471548d7542f1b99f
王焱辉 <wyh_19@163.com>
2019-02-03 14:52:11 +0800

    练习

---
+++ 4-队列/4-循环队列.js
@@ -0,0 +1,43 @@
+function Queue() {
+    let items = [];
+
+    this.enqueue = function (element) {
+        items.push(element);
+    }
+
+    this.dequeue = function () {
+        return items.shift();
+    }
+    this.front = function () {
+        return items[0];
+    }
+    this.isEmpty = function () {
+        return items.length == 0;
+    }
+    this.size = function(){
+        return items.length
+    }
+    this.print = function () {
+        console.log(items.toString());
+    }
+
+}
+
+
+function hotPotato(nameList, num) {
+    let queue = new Queue(); 
+    for (let i = 0; i < nameList.length; i++) {
+        queue.enqueue(nameList[i]); 
+    }
+    let eliminated = '';
+    while (queue.size() > 1) {
+        for (let i = 0; i < num; i++) {
+            queue.enqueue(queue.dequeue()); 
+        }
+        eliminated = queue.dequeue();
+    }
+    return queue.dequeue();
+}
+let names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl']; 
+let winner = hotPotato(names, 7); 
+console.log('The winner is: ' + winner);
