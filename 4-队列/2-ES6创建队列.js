a3ccbfbef65c60a395c02143ae2a4a2c413659cb
王焱辉 <wyh_19@163.com>
2019-02-03 10:36:51 +0800

    练习

---
+++ 4-队列/2-ES6创建队列.js
@@ -0,0 +1,19 @@
+let Queue2 = (function () {
+    const items = new WeakMap();
+    class Queue2 {
+        constructor() {
+            items.set(this, []);
+        }
+        enqueue(element) {
+            let q = items.get(this);
+            q.push(element);
+        }
+        dequeue() {
+            let q = items.get(this);
+            let r = q.shift();
+            return r;
+        }
+        //其他方法 
+    }
+    return Queue2;
+})();
